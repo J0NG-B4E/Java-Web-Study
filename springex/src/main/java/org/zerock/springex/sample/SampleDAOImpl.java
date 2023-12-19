f68c92c0fd8f8cff0a1a1b0e96fd55603051f5bd
wjdwhdqo <75736901+B3LLz@users.noreply.github.com>
2023-12-20 01:58:35 +0900

    spring webMVC test.html

--- springex/src/main/java/org/zerock/springex/sample/SampleDAOImpl.java
+++
@@ -1,9 +0,0 @@
-package org.zerock.springex.sample;
-
-import org.springframework.stereotype.Repository;
-import org.springframework.beans.factory.annotation.Qualifier;
-
-@Repository
-@Qualifier("normal")
-public class SampleDAOImpl implements SampleDAO {
-}
