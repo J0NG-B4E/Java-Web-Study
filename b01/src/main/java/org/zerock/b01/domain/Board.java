2112ca81cfca154751f34ac5a6a03df57e5b17b7
wjdwhdqo <75736901+B3LLz@users.noreply.github.com>
2023-12-24 04:36:48 +0900

    jpa start

---
+++ b01/src/main/java/org/zerock/b01/domain/Board.java
@@ -0,0 +1,26 @@
+package org.zerock.b01.domain;
+
+// spring boot version 3에서는 패키지 명이 javax에서 jakarta로 변경
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Board extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bno;
+
+    @Column(length = 500, nullable = false) // 컬럼의 길이와 null 허용여부
+    private String title;
+
+    @Column(length = 2000, nullable = false)
+    private String content;
+
+    @Column(length = 50, nullable = false)
+    private String writer;
+}
